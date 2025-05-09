bb1b3f40ae0d7f85e6e2c8fd1bd30a5e01565730
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-08 20:32:36 -0400

    ratelimit from redis

--- middleware.js
+++ middleware.js
@@ -1,12 +1,32 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 import { createRouteMatcher } from "@clerk/nextjs/server";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+import { NextResponse } from "next/server";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.TOKEN,
+});
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(10, "1m"),
+  analytics: true,
+});
 
 const isPublicRoute = createRouteMatcher(["/", "/stack"]);
 
 export default clerkMiddleware(async (auth, req) => {
+  const ip = req.ip || req.headers.get("x-forwarded-for");
+
+  const { success } = await ratelimit.limit(ip.toString());
+  if (!success) {
+    return NextResponse.json("Too Many Requests");
+  }
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
+  return NextResponse.next();
 });
 
 export const config = {
