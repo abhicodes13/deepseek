512896b4220839d90b6cb7ba2d9882716e7a59a6
AbhiCodes <chhettriabhishek9@gmail.com>
2026-08-18 11:55:40 -0400

    test redis connection

--- middleware.js
+++ middleware.js
@@ -1,9 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
 
 const isPublicRoute = createRouteMatcher(["/", "/stack"]);
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
 export default clerkMiddleware(async (auth, req) => {
+  const result = await redis.ping();
+  console.log("VERCEL REDIS:", result);
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
